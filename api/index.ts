48aebdddbac6b0818956cd628507b767a07c6a0f
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-20 12:36:41 +0800

    chore: init

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import { defineEventHandler } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  const body = await useBody(event)
+  const data = {
+    // @ts-ignore
+    url: event.req.url,
+    // @ts-ignore
+    method: event.req.method,
+    body,
+  }
+  return data
+})
