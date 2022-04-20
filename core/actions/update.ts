94a3e4014efcf5bae788706c74a17e004506bf2f
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-20 16:44:40 +0800

    chore: update

--- core/actions/update.ts
+++ core/actions/update.ts
@@ -1,10 +1,10 @@
 import type { Issue } from '@octokit/webhooks-types'
 import type { Context } from '../types'
 import { getExistingUpdateComment, info, updateComment } from '../utils'
-import { COMMENT_FORWARD_ISSUE, COMMENT_UPDATE_COMMENT } from '../constants'
+import { COMMENT_FORWARD_ISSUE, COMMENT_UPDATE_COMMENT, SAME_ISSUE_RATE } from '../constants'
 import { readConfig } from '../config'
 
-const inProgressMap = new Set<string>()
+const inProgressMap = new Map<string, number>()
 
 /**
  * Check issue labels and create forward issue if needed
@@ -16,52 +16,51 @@ export async function updateIssue(ctx: Context, issue: Issue) {
     return info('>>> The issue is not open')
 
   const key = `${ctx.source.owner}/${ctx.source.repo}/${issue.number}`
-  if (inProgressMap.has(key))
-    return info(`>>> Another action in progress for ${key}`)
-  inProgressMap.add(key)
+  if (Date.now() - (inProgressMap.get(key) || 0) < SAME_ISSUE_RATE)
+    return info(`>>> Throttled for ${key}`)
+  inProgressMap.set(key, Date.now())
 
-  try {
-    const { octokit } = ctx
-    const { tag: triggerTag, upstream: upstreamMap } = await readConfig(ctx, true)
+  const { octokit } = ctx
+  const { tag: triggerTag, upstream: upstreamMap } = await readConfig(ctx, true)
 
-    const number = issue.number
-    const labels: string[] = (issue.labels || [])
-      .map((i: any) => i && i.name)
-      .filter(Boolean)
+  const number = issue.number
+  const labels: string[] = (issue.labels || [])
+    .map((i: any) => i && i.name)
+    .filter(Boolean)
 
-    info('-------')
-    info(`Checkout issue: ${issue.html_url} [${labels.join(', ')}]`)
+  info('-------')
+  info(`Checkout issue: ${issue.html_url} [${labels.join(', ')}]`)
 
-    if (!labels.includes(triggerTag))
-      return info(`>>> Non-target issue (no "${triggerTag}" tag)`)
+  if (!labels.includes(triggerTag))
+    return info(`>>> Non-target issue (no "${triggerTag}" tag)`)
 
-    const targets = Object.keys(upstreamMap).filter(i => labels.includes(i))
-    if (!targets.length)
-      return info(`>>> No upstream tag found (supports: ${Object.keys(upstreamMap).join(', ')})`)
-    if (targets.length !== 1)
-      return info(`>>> Multiple upstream tags found (${targets.join(', ')}), failed to determine which one to use`)
+  const targets = Object.keys(upstreamMap).filter(i => labels.includes(i))
+  if (!targets.length)
+    return info(`>>> No upstream tag found (supports: ${Object.keys(upstreamMap).join(', ')})`)
+  if (targets.length !== 1)
+    return info(`>>> Multiple upstream tags found (${targets.join(', ')}), failed to determine which one to use`)
 
-    const target = targets[0]
-    const upstreamName = upstreamMap[target]
-    const upstreamIssueLink = `https://github.com/${upstreamName}/issues/`
+  const target = targets[0]
+  const upstreamName = upstreamMap[target]
+  const upstreamIssueLink = `https://github.com/${upstreamName}/issues/`
 
-    info(`
+  info(`
 tag:  ${target}
 upstream: ${upstreamName}
 `.trim())
 
-    let forwardIssueNo: number | undefined
-    const { comment: existing } = (await getExistingUpdateComment(ctx, number)) || {}
+  let forwardIssueNo: number | undefined
+  const { comment: existing } = (await getExistingUpdateComment(ctx, number)) || {}
 
-    info(`Existing Comment: ${existing?.html_url}`)
+  info(`Existing Comment: ${existing?.html_url}`)
 
-    const [owner, repo] = upstreamName.split('/')
-    if (!existing) {
-      const { data: forwarded } = await octokit.rest.issues.create({
-        owner,
-        repo,
-        title: issue.title,
-        body: `
+  const [owner, repo] = upstreamName.split('/')
+  if (!existing) {
+    const { data: forwarded } = await octokit.rest.issues.create({
+      owner,
+      repo,
+      title: issue.title,
+      body: `
 ${COMMENT_FORWARD_ISSUE(ctx.source.owner, ctx.source.repo, issue.number)}
 
 <table><td><img width="760" height="0" src="${issue.html_url}">
@@ -73,48 +72,44 @@ ${COMMENT_FORWARD_ISSUE(ctx.source.owner, ctx.source.repo, issue.number)}
 
 ${issue.body || ''}
     `,
-      })
-      info(`Issue Created: ${forwarded.html_url}`)
-      forwardIssueNo = forwarded.number
+    })
+    info(`Issue Created: ${forwarded.html_url}`)
+    forwardIssueNo = forwarded.number
 
-      const { data: comment } = await updateComment(ctx, number, `
+    const { data: comment } = await updateComment(ctx, number, `
 ${COMMENT_UPDATE_COMMENT(owner, repo, forwardIssueNo)}\n
 Upstream issue created:
 - ${upstreamIssueLink}${forwardIssueNo}
 `.trim(),
-      existing)
-      info(`Updating comment: ${comment.html_url}`)
-    }
+    existing)
+    info(`Updating comment: ${comment.html_url}`)
+  }
 
-    if (existing?.body) {
-      const index = existing.body.indexOf(upstreamIssueLink)
-      if (index) {
-        const number = +(existing.body.slice(index + upstreamIssueLink.length).match(/^\d+/)?.[0] || 0)
-        if (number)
-          forwardIssueNo = number
-      }
-    }
-
-    info(`Forward Issue: ${upstreamIssueLink}${forwardIssueNo}\n`)
-
-    if (forwardIssueNo) {
-      const { data: issue } = await octokit.rest.issues.get({
-        repo,
-        owner,
-        issue_number: forwardIssueNo,
-      })
-
-      if (issue.state === 'closed') {
-        info('---Close---')
-        await octokit.rest.issues.update({
-          ...ctx.source,
-          issue_number: number,
-          state: 'closed',
-        })
-      }
+  if (existing?.body) {
+    const index = existing.body.indexOf(upstreamIssueLink)
+    if (index) {
+      const number = +(existing.body.slice(index + upstreamIssueLink.length).match(/^\d+/)?.[0] || 0)
+      if (number)
+        forwardIssueNo = number
     }
   }
-  finally {
-    inProgressMap.delete(key)
+
+  info(`Forward Issue: ${upstreamIssueLink}${forwardIssueNo}\n`)
+
+  if (forwardIssueNo) {
+    const { data: issue } = await octokit.rest.issues.get({
+      repo,
+      owner,
+      issue_number: forwardIssueNo,
+    })
+
+    if (issue.state === 'closed') {
+      info('---Close---')
+      await octokit.rest.issues.update({
+        ...ctx.source,
+        issue_number: number,
+        state: 'closed',
+      })
+    }
   }
 }
--- core/constants.ts
+++ core/constants.ts
@@ -5,3 +5,5 @@ export const COMMENT_FORWARD_ISSUE_RE = /<!-- upissues-forward-issue https:\/\/g
 
 export const COMMENT_UPDATE_COMMENT = (owner: string, repo: string, number: number) => `<!-- upissues-update-comment https://github.com/${owner}/${repo}/issues/${number} -->`
 export const COMMENT_UPDATE_COMMENT_RE = /<!-- upissues-update-comment https:\/\/github.com\/([^\/]+)\/([^\/]+)\/issues\/(\d+) -->/
+
+export const SAME_ISSUE_RATE = 10_000
