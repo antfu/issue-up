f8e6b912b090e829cee248768528f6e02626a8cb
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-20 15:57:34 +0800

    chore: update

--- core/index.ts
+++ core/index.ts
@@ -1,6 +1,8 @@
 import { updateIssue } from './actions/update'
 import { updateUpstream } from './actions/upstreamClose'
+import { readConfig } from './config'
 import type { Context } from './types'
+import { info } from './utils'
 export * from './types'
 
 export async function runAction(ctx: Context) {
@@ -8,6 +10,10 @@ export async function runAction(ctx: Context) {
 
   if (await updateUpstream(ctx))
     return
+
+  const config = await readConfig(ctx)
+  if (!config)
+    return info('No config found')
 
   if ('issue' in event)
     await updateIssue(ctx, event.issue)
